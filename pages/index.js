56c2e1ce909e521de185938986a616102395129c
Gary Fuller <fullermotion@users.noreply.github.com>
2022-11-10 17:22:07 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Gary's test of Next.js on Azure!
         </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
